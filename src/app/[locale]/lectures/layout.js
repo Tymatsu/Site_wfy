0413a3bd6ed3c4a6f9e1bb554334860cb0de73d7
Tymatsu <91764148+Tymatsu@users.noreply.github.com>
2025-04-12 01:08:43 -0300

    Create layout.js

---
+++ src/app/[locale]/lectures/layout.js
@@ -0,0 +1,12 @@
+// export const metadata = {
+//   title: 'WFY',
+//   description: 'What Flows You',
+// }
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
